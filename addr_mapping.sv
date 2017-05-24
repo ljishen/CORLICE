d93b01965653b1c0b0ebee71e8f071d683164417
Jianshen Liu <jliu120@ucsc.edu>
2017-05-24 08:42:43 +0000

    Tune module structure

--- addr_mapping.sv
+++ addr_mapping.sv
@@ -6,36 +6,58 @@ module addr_maping(in, out);
 
    input in;
    output out;
+
+   default_hash dh[1:0]();
+
+   int num_buckets;
+   always @(num_buckets) begin
+      dh[0].update(num_buckets);
+      dh[1].update(num_buckets);
+   end
    
-   default_hash dh();
+   initial begin
+      num_buckets = 4;
+   end
    
-   task generate_hash_functions;
-      
-   endtask
+   /* Adds a new sva/spa pair to the hash map. If the key already existed,
+    * its old value is displaced and the new value is written in its stead.
+    */
+   function addr_bits put(input addr_bits spa, sva);
+      /* Check whether this value already exists. If so, just displace its
+       * old value and hand it back.
+       */
+      repeat(2)
+         begin
+            
+         end
+   endfunction
 
 endmodule
 
 
 module default_hash ();
-   const int LG_NUM_BUCKETS = 2;   // 2^LG_NUM_BUCKETS is the number of buckets we're hashing into.
+   int lg_num_buckets;
+   addr_bits coe_a, coe_b;
 
-   addr_bits coe_a = {$urandom, $urandom};
-   addr_bits coe_b = {$urandom, $urandom};
-
-   function addr_bits hash;
-      input addr_bits sva;
+   function addr_bits hash(input addr_bits sva);
       bit [`ADDR_WIDTH/2-1:0] upper, lower, mask;
    
       upper = sva >> (`ADDR_WIDTH / 2);
       mask = (1 << (`ADDR_WIDTH / 2)) - 1;
       lower = sva & mask;
 
-      hash = (upper * coe_a + lower * coe_b) >> (`ADDR_WIDTH - LG_NUM_BUCKETS);
+      hash = (upper * coe_a + lower * coe_b) >> (`ADDR_WIDTH - lg_num_buckets);
    endfunction
-   
-   task refresh;
+
+   task update(input int num_buckets);
+      automatic int lg_val = -1;
+      while (num_buckets > 0) begin
+         num_buckets = num_buckets >> 1;
+         lg_val = lg_val + 1;
+      end
+      lg_num_buckets = lg_val;
+
       coe_a = {$urandom, $urandom};
       coe_b = {$urandom, $urandom};
    endtask
-   
 endmodule
--- addr_mapping_tb.sv
+++ addr_mapping_tb.sv
@@ -2,15 +2,16 @@
 
 module addr_mapping_tb();
 
-   addr_bits coe_a, coe_b, val;
+   addr_bits val;
 
    task print;
       begin
          val = dh.hash(`ADDR_WIDTH'h aaaaaaaabbbbbbbb);
-         
+
          $display("sva is %h", dh.hash.sva);
          $display("upper is %h", dh.hash.upper);
          $display("lower is %h", dh.hash.lower);
+         $display("lg_num_buckets is %d", dh.lg_num_buckets);
          $display("coe_a is %h", dh.coe_a);
          $display("coe_b is %h", dh.coe_b);
          $display("val is %d", val);
@@ -18,15 +19,21 @@ module addr_mapping_tb();
    endtask
 
    default_hash dh();
+   
+   int num_buckets;
+   always @(num_buckets) begin
+      dh.update(num_buckets);
+   end
 
    initial
    begin
+      num_buckets = 4; #1
+      print();
+      
+      num_buckets = 8; #1
       print();    #1
       
-      dh.refresh();
-      print();    #1
-      
-      dh.refresh();
+      num_buckets = 16; #1
       print();
    end
 
