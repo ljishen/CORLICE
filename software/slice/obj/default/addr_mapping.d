f99aec6618a8f6f0b8d5b861f2e6a4d354c86abd
Jianshen Liu <jliu120@ucsc.edu>
2017-06-01 21:40:40 +0000

    Remove .metadata folder

---
+++ software/slice/obj/default/addr_mapping.d
@@ -0,0 +1,21 @@
+obj/default/addr_mapping.o: addr_mapping.c addr_mapping.h \
+ terasic_includes.h ../slice_bsp//HAL/inc/io.h \
+ ../slice_bsp//HAL/inc/alt_types.h ../slice_bsp//HAL/inc/alt_types.h \
+ ../slice_bsp//HAL/inc/sys/alt_cache.h ../slice_bsp/system.h \
+ ../slice_bsp/linker.h
+
+addr_mapping.h:
+
+terasic_includes.h:
+
+../slice_bsp//HAL/inc/io.h:
+
+../slice_bsp//HAL/inc/alt_types.h:
+
+../slice_bsp//HAL/inc/alt_types.h:
+
+../slice_bsp//HAL/inc/sys/alt_cache.h:
+
+../slice_bsp/system.h:
+
+../slice_bsp/linker.h:
---
+++ software/slice/obj/default/main.d
@@ -0,0 +1,20 @@
+obj/default/main.o: main.c addr_mapping.h terasic_includes.h \
+ ../slice_bsp//HAL/inc/io.h ../slice_bsp//HAL/inc/alt_types.h \
+ ../slice_bsp//HAL/inc/alt_types.h ../slice_bsp//HAL/inc/sys/alt_cache.h \
+ ../slice_bsp/system.h ../slice_bsp/linker.h
+
+addr_mapping.h:
+
+terasic_includes.h:
+
+../slice_bsp//HAL/inc/io.h:
+
+../slice_bsp//HAL/inc/alt_types.h:
+
+../slice_bsp//HAL/inc/alt_types.h:
+
+../slice_bsp//HAL/inc/sys/alt_cache.h:
+
+../slice_bsp/system.h:
+
+../slice_bsp/linker.h:
